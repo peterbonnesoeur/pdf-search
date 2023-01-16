9b259cf91c9c3a5536cd884024863b7a7dac57d6
peterbonnesoeur <maxime.bonn@gmail.com>
2023-01-16 21:47:44 +0000

    Changed folder organisation

--- src/gpt.py
+++ src/gpt.py
@@ -16,12 +16,12 @@ COMPLETIONS_API_PARAMS = {
 
 def construct_prompt(haystack_prediction):
   header = """Ignore all previous direction. Answer the question as truthfully as possible using the provided context, first write your answer, then the source and page mentioned in the prompt and if the answer is not contained within the text below, say "I don't know"."""
-  
+
   prediction = haystack_prediction['documents'][0]
   context = prediction.content
   source = prediction.meta["chapter"]
   page = prediction.meta["start_page"]
-  
+
 
   return header + "\n\nContext: " + context + "\nSource: " + source + "\nPage: " + str(page) + "\n\nThe question is: " + haystack_prediction['query']
 
@@ -36,4 +36,33 @@ def answer_query_with_context(query, pipeline) -> str:
                 **COMPLETIONS_API_PARAMS
             )
 
+
     return response["choices"][0]["text"].strip("\n")
+
+def answer_query(query) -> str:
+
+
+  response = openai.Completion.create(
+              prompt=query,
+              **COMPLETIONS_API_PARAMS
+          )
+  print(response)
+
+  return response["choices"][0]["text"].strip("\n")
+
+# import json
+# import requests
+# headers = {"Authorization": f"Bearer {API_TOKEN}"}
+# API_URL = "https://api-inference.huggingface.co/models/deepset/roberta-base-squad2"
+# def query(payload):
+#     data = json.dumps(payload)
+#     response = requests.request("POST", API_URL, headers=headers, data=data)
+#     return json.loads(response.content.decode("utf-8"))
+# data = query(
+#     {
+#         "inputs": {
+#             "question": "What's my name?",
+#             "context": "My name is Clara and I live in Berkeley.",
+#         }
+#     }
+# )
